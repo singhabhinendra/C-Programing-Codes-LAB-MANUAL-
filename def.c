f34f1ddbd6c8557d1e473eea09b3e386d071493c
singhabhinendra <122248123+singhabhinendra@users.noreply.github.com>
2023-09-21 16:58:57 +0530

    Create def.c

---
+++ def.c
@@ -0,0 +1,8 @@
+//progam in c ,calculate the area of circle using the #define preprocessor and getch() function.
+#define pi 3.14
+int main(){
+    float area,radius=4;
+    area=radius*radius*pi;
+    printf("%f",area);
+    getch();
+}
